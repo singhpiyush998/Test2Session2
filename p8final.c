d7084b7875b43df6383a8d86a39c3806ea23b339
Piyush Singh <65072631+singhpiyush998@users.noreply.github.com>
2022-03-25 10:53:27 +0000

    p8final done gracefully

--- p8final.c
+++ p8final.c
@@ -25,47 +25,59 @@ void output(Polygon p);
 
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 
 typedef struct point
 {
-    float x, y;
+    float start_end_x, start_end_y;
 } Point;
+
 
 typedef struct line
 {
-    Point p1, p2;
+    Point p;
     float distance;
 } Line;
 
 typedef struct polygon
 {
     int sides;
-    Line l[100];
+    Line* l;
     float perimeter;
 } Polygon;
 
+int input_n();
+Line input_line();
+void input_n_lines(int n, Line* l);
+int input_polygon(Polygon* p);
+float find_distance(Point a, Point b);
+void find_n_distance(int n, Line* l);
+void find_perimeter(Polygon* p);
+void output(Polygon p);
+
+int main(void)
+{
+    Polygon p;
+    input_polygon(&p);
+    find_perimeter(&p);
+    output(p);
+
+    return 0;
+}
+
 int input_n()
 {
-    int sides;
-    printf("Enter the sides of a polygon: ");
-    scanf("%d", &sides);
-    return sides;
+    int n;
+    printf("Enter the number of sides of the polygon: ");
+    scanf("%d", &n);
+    return n;
 }
 
-Point input_point()
-{
-    Point p;
-    scanf("%f%f", &p.x, &p.y);
-    return p;
-}
-
-Line input_line()
+Line input_line(int n)
 {
     Line l;
-    printf("Enter the first point:\n");
-    l.p1 = input_point();
-    printf("Enter the second point:\n");
-    l.p2 = input_point();
+    printf("Enter the coordinates of point %d (x,y): \n", n);
+    scanf("%f%f", &l.p.start_end_x, &l.p.start_end_y);
     return l;
 }
 
@@ -73,51 +85,44 @@ void input_n_lines(int n, Line* l)
 {
     for (int i = 0; i < n; i++)
     {
-        printf("Line number %d\n", i + 1);
-        l[i] = input_line();
+        l[i] = input_line(i + 1);
     }
 }
 
 int input_polygon(Polygon* p)
 {
     p->sides = input_n();
+    p->l = malloc(3 * p->sides * sizeof(float));
     input_n_lines(p->sides, p->l);
+    p->perimeter = 0;
 }
 
-void find_length(Line* l)
+float find_distance(Point a, Point b)
 {
-    l->distance = sqrt((l->p1.x - l->p2.x) * (l->p1.x - l->p2.x) + (l->p1.y - l->p2.y) * (l->p1.y - l->p2.y));
+    return sqrt((a.start_end_x - b.start_end_x) * (a.start_end_x - b.start_end_x) + (a.start_end_y - b.start_end_y) * (a.start_end_y - b.start_end_y));
 }
 
-
-void find_n_lengths(int n, Line* l)
+void find_n_distance(int n, Line* l)
 {
-    for (int i = 0; i < n; i++)
+    int i;
+    for (i = 0; i < n - 1; i++)
     {
-        find_length(&l[i]);
+        l[i].distance = find_distance(l[i].p, l[i + 1].p);
     }
+    l[i].distance = find_distance(l[i].p, l[0].p);
 }
 
 void find_perimeter(Polygon* p)
 {
-    find_n_lengths(p->sides, p->l);
-    p->perimeter = 0;
+    find_n_distance(p->sides, p->l);
     for (int i = 0; i < p->sides; i++)
     {
         p->perimeter += p->l[i].distance;
     }
+    free(p->l);
 }
 
 void output(Polygon p)
 {
-    printf("The perimeter of given polygon is: %.2f\n", p.perimeter);
-}
-
-int main()
-{
-    Polygon p;
-    input_polygon(&p);
-    find_perimeter(&p);
-    output(p);
-    return 0;
+    printf("The perimeter of the polynomial is: %f\n", p.perimeter);
 }
